f03c088c9a8647f24458497a6de0fe96b7a24ed9
JavierObeso <56382307+JavierObeso@users.noreply.github.com>
2019-11-04 14:58:43 -0700

    Update Marcador.java

--- TimbiricheObjetosNegocio/src/objetosNegocio/Marcador.java
+++ TimbiricheObjetosNegocio/src/objetosNegocio/Marcador.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package objetosNegocio;
 
 /**
@@ -11,53 +6,18 @@ package objetosNegocio;
  */
 public class Marcador {
 
-    private int pJ1, pJ2, pJ3, pJ4;
+    private int puntajes[];
 
-    public Marcador(int pJ1, int pJ2, int pJ3, int pJ4) {
-        this.pJ1 = pJ1;
-        this.pJ2 = pJ2;
-        this.pJ3 = pJ3;
-        this.pJ4 = pJ4;
+    public Marcador(int [] puntajes) {
+        this.puntajes = puntajes;
     }
-    
-    public int[] obtenerPuntajes(){
-        int[] puntajes = new int[4];
-        puntajes[0] = pJ1;
-        puntajes[1] = pJ2;
-        puntajes[2] = pJ3;
-        puntajes[3] = pJ4;
+
+    public int[] getPuntajes() {
         return puntajes;
     }
 
-    public int getpJ1() {
-        return pJ1;
+    public void setPuntajes(int[] puntajes) {
+        this.puntajes = puntajes;
     }
 
-    public void setpJ1(int pJ1) {
-        this.pJ1 = pJ1;
-    }
-
-    public int getpJ2() {
-        return pJ2;
-    }
-
-    public void setpJ2(int pJ2) {
-        this.pJ2 = pJ2;
-    }
-
-    public int getpJ3() {
-        return pJ3;
-    }
-
-    public void setpJ3(int pJ3) {
-        this.pJ3 = pJ3;
-    }
-
-    public int getpJ4() {
-        return pJ4;
-    }
-
-    public void setpJ4(int pJ4) {
-        this.pJ4 = pJ4;
-    }
 }
