fa30fbd49c6edb52dda3265230b255a916be9b97
JavierObeso <56382307+JavierObeso@users.noreply.github.com>
2019-11-04 14:57:48 -0700

    Update Linea.java

--- TimbiricheObjetosNegocio/src/objetosNegocio/Linea.java
+++ TimbiricheObjetosNegocio/src/objetosNegocio/Linea.java
@@ -1,68 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package objetosNegocio;
-
-import java.awt.Color;
-import javax.swing.JButton;
-import javax.swing.plaf.metal.MetalButtonUI;
 
 /**
  * Clase para crear líneas.
  * @author Javier Obeso, J. Armando Méndez, J. Eduardo Montoya, L. Enrique Mendoza
  */
-public class Linea extends JButton{
+public class Linea extends Forma{
     
-    public Linea(){}
-    
-    public Linea(boolean cuadro){
-        this.cuadro = cuadro;
-    }
-    
-    //Dice si la línea ya fue tomada.
-    private boolean tomado = false;
-    
-    //Dice si ya se completó un cuadro.
-    private boolean cuadro = false;
-    
-    //Guarda el jugador dueño del cuadro.
-    private Jugador owner;
-    
-    public boolean getTomado(){
-        return tomado;
-    }
-    public void setTomado(boolean tomado){
-        this.tomado = tomado;
-    }
-    
-    public Jugador getOwner(){
-        return owner;
-    }
-    public void setOwner(Jugador owner){
-        this.owner = owner;
-        try{
-            if (cuadro) {
-                this.setText(Character.toString(owner.getNickname().charAt(0)));
-                this.setBackground(owner.getColor());
-                this.setUI(new MetalButtonUI() {
-                    protected Color getDisabledTextColor() {
-                    return Color.black;
-                    }
-                });
-            }
-        }
-        catch(Exception e){
-            this.setText(null);
-        }
-    }
-    
-    public boolean getCuadro(){
-        return cuadro;
-    }
-    public void setCuadro(boolean cuadro){
-        this.cuadro = cuadro;
-    }
+    public Linea(boolean tomado, Jugador owner){
+        super(tomado, owner);
+    }   
     
 }
+
