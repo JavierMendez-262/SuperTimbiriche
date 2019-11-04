2bb3b2f1bedbdb63e4ccfd91ad844b0ec19565a3
JavierObeso <56382307+JavierObeso@users.noreply.github.com>
2019-11-04 14:56:54 -0700

    Add files via upload

---
+++ TimbiricheObjetosNegocio/src/objetosNegocio/Cuadro.java
@@ -0,0 +1,14 @@
+package objetosNegocio;
+
+/**
+ * Clase que crea cuadros.
+ * @author Javier Obeso, J. Armando Méndez, J. Eduardo Montoya, L. Enrique Mendoza
+ */
+public class Cuadro extends Forma{
+    
+    public Cuadro (){}
+    
+    public Cuadro(boolean tomado, Jugador owner) {
+        super(tomado, owner);
+    }   
+} 
